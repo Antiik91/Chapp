0563e88a47096568d43dae6f20cc287dcecb24e5
Antiik91 <antiik91@gmail.com>
2016-12-21 12:16:27 +0200

    Shows now when user is connected and disconnected from channel, started working with support of nicknames

--- index.js
+++ index.js
@@ -6,10 +6,24 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+var clients = [];
 io.on('connection', function(socket) {
+    ;
     console.log('a user is connected');
+    socket.on("setUsername", function(data){
+       if(clients.indexOf(data) > -1) {
+           clients.push(data);
+           socket.emit('userSet', {username: data});
+       }
+       else {
+           socket.emit('userExists', data + 'Valitettavasti käyttäjänimi on jo varattu :()')
+       }
+    });
+    io.sockets.emit('broadcast', {description: ' user joined in channel'});
+
     socket.on('disconnect', function(){
         console.log('user disconnected');
+        io.sockets.emit('broadcast', {description: ' user left from channel'});
     });
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
