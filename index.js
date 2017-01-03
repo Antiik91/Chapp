fb0fa1995688b227f6c250ff516a30e68368eee8
Antiik91 <antiik91@gmail.com>
2017-01-03 15:35:08 +0200

    Started with multiroom support socket.get method's are gone, will have to look a new way to use them

--- index.js
+++ index.js
@@ -7,19 +7,30 @@ app.get('/', function(req, res){
 });
 
 var clients = [];
-io.on('connection', function(socket) {
+io.sockets.on('connection', function(socket) {
     console.log('a user is connected');
     socket.on('setUsername', function(data){
        if(clients.indexOf(data) > -1) {
            socket.emit('userTaken', data + ' Valitettavasti on jo varattu');
 //           console.log(data +' varattu');
-       }
-       else {
+       } else {
            clients.push(data);
            socket.emit('setUser', {username: data});
 //           console.log(data +' Vapaana');
            io.sockets.emit('broadcast', {description: data + ' Liittyi kanavalle'});
        }
+    });
+
+    socket.on('join', function(channel, ack) {
+       socket.get('channel', function(err,oldChannel){
+         if(oldChannel) {
+             socket.leave('oldChannel');
+         }
+         socket.set('channel', channel, function() {
+            socket.join(channel);
+            ack();
+         });
+       });
     });
     socket.on('disconnect', function(data){
         var u =  clients.indexOf(data);
@@ -27,8 +38,20 @@ io.on('connection', function(socket) {
         console.log(' user disconnected');
         io.sockets.emit('broadcast', {description: 'Käyttäjä lähti kanavalta'});
     });
-    socket.on('chat message', function(user, data){
-        io.sockets.emit('broadcast', {description: user +": "+ data});
+    socket.on('chat message', function(user, data, ack){
+        socket.get('channel', function(err,channel){
+            if(err) {
+                socket.emit('error',err);
+            }
+            else if(channel) {
+               io.socket.broadcast.to(channel).emit('broadcast', {description: user +": "+ data});
+               ack();
+            } else {
+                socket.emit('error', 'Ei kanavaa valittuna');
+                console.log('Something happened in chat message section.');
+            }
+        });
+
 //        console.log('viesti ' + user + " "+ data);
     });
 });
